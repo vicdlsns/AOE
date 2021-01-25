f08f865201706b962b841763702db1a36fe54e99
vdl149 <30540488+vicdlsns@users.noreply.github.com>
2021-01-24 23:29:55 -0300

    Update AOE.py

--- AOE.py
+++ AOE.py
@@ -14,9 +14,10 @@ def copia_matriz(m):
     return c
 
 def AOE(m):
-    """Recebe uma matriz de incidencia onde as linhas sao as equacoes e as colunas as variaveis
-    Retorna uma matriz no formato [[equação,variável]] ordenada e uma lista de variáveis que sobraram
-    'F' significa 'final'"""
+    """Recebe uma matriz de incidencia (lista de listas) onde as linhas sao as equacoes e as colunas as variaveis.
+    A matriz deve conter '1' onde a variável estriver presente e '0' caso contrário.
+    Retorna uma matriz com listas no formato [[equação,variável]] ordenada e uma lista de variáveis que sobraram.
+    'F' significa 'final'."""
     #linhas - equacoes
     #colunas - variaveis
     e=len(m)
@@ -86,50 +87,59 @@ def AOE(m):
 
     return L, s
 
-#exercicio master 3
-a=[0]*14
-m=[]
-for i in range(14):
-    m.append(a[:])
-
-m[0][0]=1
-m[0][2]=1
-m[1][1]=1
-m[2][2]=1
-m[2][3]=1
-m[3][2]=1
-m[3][3]=1
-m[4][3]=1
-m[4][4]=1
-m[4][5]=1
-m[5][4]=1
-m[5][5]=1
-m[6][5]=1
-m[6][6]=1
-m[6][7]=1
-m[6][8]=1
-m[7][9]=1
-m[8][7]=1
-m[8][10]=1
-m[9][7]=1
-m[9][10]=1
-m[10][10]=1
-m[10][11]=1
-m[10][12]=1
-m[11][11]=1
-m[11][12]=1
-m[12][6]=1
-m[12][11]=1
-m[12][13]=1
-m[13][6]=1
-m[13][13]=1
-
-a,b=AOE(m)
-print("Ordenamento")
-print("Eq, Var")
-print("Obs: 'F' significa 'final'")
-for x in a:
-    print(x)
-print()
-print("Sobra")
-print(b)
+def master3()
+    #exemplo do exercicio master - etapa 3
+    
+    #cria matriz de zeros
+    a=[0]*14
+    m=[]
+    for i in range(14):
+        m.append(a[:])
+    
+    #coloca '1' nos locais certos
+    m[0][0]=1
+    m[0][2]=1
+    m[1][1]=1
+    m[2][2]=1
+    m[2][3]=1
+    m[3][2]=1
+    m[3][3]=1
+    m[4][3]=1
+    m[4][4]=1
+    m[4][5]=1
+    m[5][4]=1
+    m[5][5]=1
+    m[6][5]=1
+    m[6][6]=1
+    m[6][7]=1
+    m[6][8]=1
+    m[7][9]=1
+    m[8][7]=1
+    m[8][10]=1
+    m[9][7]=1
+    m[9][10]=1
+    m[10][10]=1
+    m[10][11]=1
+    m[10][12]=1
+    m[11][11]=1
+    m[11][12]=1
+    m[12][6]=1
+    m[12][11]=1
+    m[12][13]=1
+    m[13][6]=1
+    m[13][13]=1
+    
+    #executa o AOE
+    a,b=AOE(m)
+    
+    #escreve na tela de uma maneira mais agradável aos olhos
+    print("Ordenamento")
+    print("Eq, Var")
+    print("Obs: 'F' significa 'final'")
+    for x in a:
+        print(x)
+    print()
+    print("Sobra")
+    print(b)
+    
+master3()
